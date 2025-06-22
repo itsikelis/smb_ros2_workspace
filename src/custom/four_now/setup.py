adab08c5219b18088b435e001786b0129f5c9438
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2025-06-22 21:56:46 +0200

    Now builds

--- src/custom/four_now/setup.py
+++ src/custom/four_now/setup.py
@@ -1,26 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'src/four_now'
+package_name = "four_now"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='itsikelis',
-    maintainer_email='tsikelis.i@protonmail.com',
-    description='TODO: Package description',
-    license='BSD-2-Clause',
-    tests_require=['pytest'],
+    maintainer="itsikelis",
+    maintainer_email="tsikelis.i@protonmail.com",
+    description="TODO: Package description",
+    license="BSD-2-Clause",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'navigate_detect = src/four_now.navigate_detect:main'
-        ],
+        "console_scripts": ["navigate_detect = four_now.navigate_detect:main"],
     },
 )
