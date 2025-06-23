86fd5758c9d5da40007bf3cacd3fc0c804ae573e
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2025-06-23 18:31:56 +0200

    Changes in build map

--- src/custom/four_now/launch/build_map.launch.py
+++ src/custom/four_now/launch/build_map.launch.py
@@ -1,6 +1,9 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
+from launch.launch_description_sources import (
+    PythonLaunchDescriptionSource,
+    FrontendLaunchDescriptionSource,
+)
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -9,29 +12,30 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 
-
 def generate_launch_description():
-    
-    default_config_topics = os.path.join(get_package_share_directory('smb_bringup'), 'config', 'twist_mux_topics.yaml')
+
+    default_config_topics = os.path.join(
+        get_package_share_directory("smb_bringup"), "config", "twist_mux_topics.yaml"
+    )
     launch_args = [
         DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'
+            "use_sim_time",
+            default_value="false",
+            description="Use simulation (Gazebo) clock if true",
         ),
         DeclareLaunchArgument(
-            'use_ground_truth',
-            default_value='true',
-            description='Use ground truth odometry (true/false). If false, launches DLIO.'
+            "use_ground_truth",
+            default_value="false",
+            description="Use ground truth odometry (true/false). If false, launches DLIO.",
         ),
         DeclareLaunchArgument(
-            'config_topics',
+            "config_topics",
             default_value=default_config_topics,
-            description='Default topics config file'
+            description="Default topics config file",
         ),
     ]
-    
-    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
 
     kinematics_controller = Node(
         package="smb_kinematics",
@@ -51,53 +55,56 @@ def generate_launch_description():
 
     gmsf_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare("smb_estimator_graph_ros2"),
-                "launch",
-                "smb_estimator_graph_sim.launch.py"
-            ])
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("smb_estimator_graph_ros2"),
+                    "launch",
+                    "smb_estimator_graph_sim.launch.py",
+                ]
+            )
         ),
     )
 
     open3d_slam_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare("open3d_slam_ros"),
-                "launch",
-                "summer_school_slam_robot_launch.py"
-            ])
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("open3d_slam_ros"),
+                    "launch",
+                    "summer_school_slam_robot_launch.py",
+                ]
+            )
         ),
-        launch_arguments={
-            "use_sim_time": "true"
-        }.items(),
+        launch_arguments={"use_sim_time": "true"}.items(),
     )
 
     smb_ui = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare("smb_ui"),
-                "launch",
-                "smb_ui_sim.launch.py"
-            ])
+            PathJoinSubstitution(
+                [FindPackageShare("smb_ui"), "launch", "smb_ui_sim.launch.py"]
+            )
         ),
     )
-    
+
     twist_mux = Node(
-        package='twist_mux',
-        executable='twist_mux',
-        output='screen',
-        remappings={('/cmd_vel_out', '/cmd_vel')},
+        package="twist_mux",
+        executable="twist_mux",
+        output="screen",
+        remappings={("/cmd_vel_out", "/cmd_vel")},
         parameters=[
-            {'use_sim_time': use_sim_time},
-            LaunchConfiguration('config_topics')]
+            {"use_sim_time": use_sim_time},
+            LaunchConfiguration("config_topics"),
+        ],
     )
-    
-    return LaunchDescription([
-        *launch_args,
-        twist_mux,
-        kinematics_controller,
-        low_level_controller,
-        gmsf_launch,
-        open3d_slam_launch,
-        smb_ui
-    ])
+
+    return LaunchDescription(
+        [
+            *launch_args,
+            twist_mux,
+            kinematics_controller,
+            low_level_controller,
+            gmsf_launch,
+            open3d_slam_launch,
+            smb_ui,
+        ]
+    )
