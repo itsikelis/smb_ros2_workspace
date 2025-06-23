e58c45062c3f4d52dfbc71bf27e9408ac3d6222b
Henrique <henrique.dhcaraca@gmail.com>
2025-06-23 18:04:13 +0200

    slam on machine launch file

---
+++ src/custom/four_now/launch/build_map.launch.py
@@ -0,0 +1,103 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PythonExpression
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+
+def generate_launch_description():
+    
+    default_config_topics = os.path.join(get_package_share_directory('smb_bringup'), 'config', 'twist_mux_topics.yaml')
+    launch_args = [
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'
+        ),
+        DeclareLaunchArgument(
+            'use_ground_truth',
+            default_value='true',
+            description='Use ground truth odometry (true/false). If false, launches DLIO.'
+        ),
+        DeclareLaunchArgument(
+            'config_topics',
+            default_value=default_config_topics,
+            description='Default topics config file'
+        ),
+    ]
+    
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    kinematics_controller = Node(
+        package="smb_kinematics",
+        executable="smb_kinematics_node",
+        name="smb_kinematics_node",
+        output="screen",
+        parameters=[{"use_sim_time": True}],
+    )
+
+    low_level_controller = Node(
+        package="smb_low_level_controller",
+        executable="speed_control_node",
+        name="speed_control_node",
+        output="screen",
+        parameters=[{"use_sim_time": use_sim_time}],
+    )
+
+    gmsf_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare("smb_estimator_graph_ros2"),
+                "launch",
+                "smb_estimator_graph_sim.launch.py"
+            ])
+        ),
+    )
+
+    open3d_slam_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare("open3d_slam_ros"),
+                "launch",
+                "summer_school_slam_robot_launch.py"
+            ])
+        ),
+        launch_arguments={
+            "use_sim_time": "true"
+        }.items(),
+    )
+
+    smb_ui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare("smb_ui"),
+                "launch",
+                "smb_ui_sim.launch.py"
+            ])
+        ),
+    )
+    
+    twist_mux = Node(
+        package='twist_mux',
+        executable='twist_mux',
+        output='screen',
+        remappings={('/cmd_vel_out', '/cmd_vel')},
+        parameters=[
+            {'use_sim_time': use_sim_time},
+            LaunchConfiguration('config_topics')]
+    )
+    
+    return LaunchDescription([
+        *launch_args,
+        twist_mux,
+        kinematics_controller,
+        low_level_controller,
+        gmsf_launch,
+        open3d_slam_launch,
+        smb_ui
+    ])
