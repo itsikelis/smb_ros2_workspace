5fb62ead82e55d99c5cee5202d462f255cff2380
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2025-06-25 14:24:44 +0200

    Fix waypoint publisher executable not found

--- src/custom/four_now/setup.py
+++ src/custom/four_now/setup.py
@@ -11,7 +11,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join("share", package_name, "launch"), glob("launch/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -22,8 +22,8 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            # "navigate_detect = four_now.navigate_detect:main",
-            'publish_waypoints = four_now.waypoint_publisher:main',
+            "navigate_detect = four_now.navigate_detect:main",
+            "waypoint_publisher = four_now.waypoint_publisher:main",
         ],
     },
 )
