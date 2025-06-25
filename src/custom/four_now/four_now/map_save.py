e19c2c76a57a1924704d37fb20052844130df145
dn0908 <dahyun.diana.nam@gmail.com>
2025-06-25 19:50:13 +0000

    Edits on map save

--- src/custom/four_now/four_now/map_save.py
+++ src/custom/four_now/four_now/map_save.py
@@ -1,52 +1,53 @@
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Bool
-from sensor_msgs.msg import PointCloud2
-import sensor_msgs_py.point_cloud2 as pc2
+# import rclpy
+# from rclpy.node import Node
+# from std_msgs.msg import Bool
+# from sensor_msgs.msg import PointCloud2
+# import sensor_msgs_py.point_cloud2 as pc2
 
-class MapSave(Node):
-    def __init__(self):
-        super().__init__('map_save')
-        self.exploration_done = False
-        self.map_saved = False
+# class MapSave(Node):
+#     def __init__(self):
+#         super().__init__('map_save')
+#         # self.exploration_done = False
+#         self.map_saved = False
+#         self.overall_map_sub = None
 
-        self.create_subscription(Bool, '/exploration_finish', self.finish_callback, 10)
-        self.get_logger().info('map save ON')
+#         self.create_subscription(Bool, '/exploration_finish', self.finish_callback, 10)
+        
+#         self.get_logger().info('map save ON')
 
-    def finish_callback(self, msg):
-        if msg.data and not self.map_saved:
-            self.get_logger().info('exp end, save now')
-            self.create_subscription(PointCloud2, '/overall_map', self.map_callback, 10)
-            self.exploration_done = True
+#     def finish_callback(self, msg):
+#         if msg.data and not self.map_saved:
+#             self.get_logger().info('exp end, saving now')
+#             self.create_subscription(PointCloud2, '/overall_map', self.map_callback, 10)
 
-    def map_callback(self, msg):
-        if not self.exploration_done or self.map_saved:
-            return
+#     def map_callback(self, msg):
+#         if self.map_saved:
+#             return
 
-        points = list(pc2.read_points(msg, field_names=["x", "y", "z"], skip_nans=True))
-        filename = '/tmp/final_map.pcd'
-        with open(filename, 'w') as f:
-            f.write('# .PCD v0.7 - Point Cloud Data file format\n')
-            f.write('VERSION 0.7\n')
-            f.write('FIELDS x y z\n')
-            f.write('SIZE 4 4 4\n')
-            f.write('TYPE F F F\n')
-            f.write('COUNT 1 1 1\n')
-            f.write(f'WIDTH {len(points)}\n')
-            f.write('HEIGHT 1\n')
-            f.write('VIEWPOINT 0 0 0 1 0 0 0\n')
-            f.write(f'POINTS {len(points)}\n')
-            f.write('DATA ascii\n')
-            for pt in points:
-                f.write(f"{pt[0]} {pt[1]} {pt[2]}\n")
+#         points = list(pc2.read_points(msg, field_names=["x", "y", "z"], skip_nans=True))
+#         filename = 'final_map.pcd'
+#         with open(filename, 'w') as f:
+#             f.write('# .PCD v0.7 - Point Cloud Data file format\n')
+#             f.write('VERSION 0.7\n')
+#             f.write('FIELDS x y z\n')
+#             f.write('SIZE 4 4 4\n')
+#             f.write('TYPE F F F\n')
+#             f.write('COUNT 1 1 1\n')
+#             f.write(f'WIDTH {len(points)}\n')
+#             f.write('HEIGHT 1\n')
+#             f.write('VIEWPOINT 0 0 0 1 0 0 0\n')
+#             f.write(f'POINTS {len(points)}\n')
+#             f.write('DATA ascii\n')
+#             for pt in points:
+#                 f.write(f"{pt[0]} {pt[1]} {pt[2]}\n")
 
-        self.get_logger().info(f'saved map to {filename}')
-        self.map_saved = True
+#         self.get_logger().info(f'saved map to {filename}')
+#         self.map_saved = True
 
-def main(args=None):
-    rclpy.init(args=args)
-    rclpy.spin(MapSave())
-    rclpy.shutdown()
+# def main(args=None):
+#     rclpy.init(args=args)
+#     rclpy.spin(MapSave())
+#     rclpy.shutdown()
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
