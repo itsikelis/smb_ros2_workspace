85b18bbf4b75e9359b0ce6de288fd2a4893e794c
dn0908 <dahyun.diana.nam@gmail.com>
2025-06-25 20:36:20 +0000

    changes in map

--- convert_bag.py
+++ convert_bag.py
@@ -1,62 +1,62 @@
-import sys
-import rosbag2_py
-import open3d as o3d
+# import sys
+# import rosbag2_py
+# import open3d as o3d
 
-from pathlib import Path
-from rosidl_runtime_py.utilities import get_message
-from rclpy.serialization import deserialize_message
+# from pathlib import Path
+# from rosidl_runtime_py.utilities import get_message
+# from rclpy.serialization import deserialize_message
 
-from sensor_msgs_py.point_cloud2 import read_points
+# from sensor_msgs_py.point_cloud2 import read_points
 
-# ros2 bag record /overall_map /tf /tf_static -o exploration_bag
-# python3 convert_bag.py exploration_bag /overall_map
+# # ros2 bag record /overall_map /tf /tf_static -o exploration_bag
+# # python3 convert_bag.py exploration_bag /overall_map
 
-def get_rosbag_options(path, serialization_format='cdr'):
-    storage_options = rosbag2_py.StorageOptions(uri=path, storage_id='sqlite3')
+# def get_rosbag_options(path, serialization_format='cdr'):
+#     storage_options = rosbag2_py.StorageOptions(uri=path, storage_id='sqlite3')
 
-    converter_options = rosbag2_py.ConverterOptions(
-        input_serialization_format=serialization_format,
-        output_serialization_format=serialization_format)
+#     converter_options = rosbag2_py.ConverterOptions(
+#         input_serialization_format=serialization_format,
+#         output_serialization_format=serialization_format)
 
-    return storage_options, converter_options
+#     return storage_options, converter_options
 
 
-if len(sys.argv) < 3:
-    print("Usage: python3 bag_to_pcd.py <bagfile> <pc_topic>")
-    sys.exit(1)
+# if len(sys.argv) < 3:
+#     print("Usage: python3 bag_to_pcd.py <bagfile> <pc_topic>")
+#     sys.exit(1)
 
-bag_path = Path(sys.argv[1])
-pcd_dir = bag_path.parent / "pcd"
-if not pcd_dir.exists():
-    pcd_dir.mkdir()
-print(f"Converting {bag_path.absolute()} to pcd files...")
-pc_topic = sys.argv[2]
+# bag_path = Path(sys.argv[1])
+# pcd_dir = bag_path.parent / "pcd"
+# if not pcd_dir.exists():
+#     pcd_dir.mkdir()
+# print(f"Converting {bag_path.absolute()} to pcd files...")
+# pc_topic = sys.argv[2]
 
-reader = rosbag2_py.SequentialReader()
-storage_options, converter_options = get_rosbag_options(str(bag_path))
-reader.open(storage_options, converter_options)
+# reader = rosbag2_py.SequentialReader()
+# storage_options, converter_options = get_rosbag_options(str(bag_path))
+# reader.open(storage_options, converter_options)
 
-topic_types = reader.get_all_topics_and_types()
-type_map = {topic_types[i].name: topic_types[i].type for i in range(len(topic_types))}
-fields = ('x', 'y', 'z', 'intensity', 'ring', 'time')
+# topic_types = reader.get_all_topics_and_types()
+# type_map = {topic_types[i].name: topic_types[i].type for i in range(len(topic_types))}
+# fields = ('x', 'y', 'z', 'intensity', 'ring', 'time')
 
-total_pcd = o3d.geometry.PointCloud()
-while reader.has_next():
-    (topic, data, t) = reader.read_next()
-    if topic != pc_topic:
-        continue
-    points = []
+# total_pcd = o3d.geometry.PointCloud()
+# while reader.has_next():
+#     (topic, data, t) = reader.read_next()
+#     if topic != pc_topic:
+#         continue
+#     points = []
     
-    msg_type = get_message(type_map[topic])
-    msg = deserialize_message(data, msg_type)
-    ros_points = read_points(msg, field_names=fields, skip_nans=True)
-    for p in ros_points:
-        points.append([p[0], p[1], p[2]])
+#     msg_type = get_message(type_map[topic])
+#     msg = deserialize_message(data, msg_type)
+#     ros_points = read_points(msg, field_names=fields, skip_nans=True)
+#     for p in ros_points:
+#         points.append([p[0], p[1], p[2]])
     
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(points)
-    o3d.io.write_point_cloud(f"{pcd_dir}/{t}.pcd", pcd)
-    total_pcd += pcd
-    print(f"Saved {pcd_dir}/{t}.pcd")
+#     pcd = o3d.geometry.PointCloud()
+#     pcd.points = o3d.utility.Vector3dVector(points)
+#     o3d.io.write_point_cloud(f"{pcd_dir}/{t}.pcd", pcd)
+#     total_pcd += pcd
+#     print(f"Saved {pcd_dir}/{t}.pcd")
 
-o3d.io.write_point_cloud(f"total.pcd", total_pcd)
+# o3d.io.write_point_cloud(f"total.pcd", total_pcd)
--- src/custom/four_now/launch/map_save.launch.py
+++ src/custom/four_now/launch/map_save.launch.py
@@ -1,12 +1,12 @@
-# from launch import LaunchDescription
-# from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
-# def generate_launch_description():
-#     return LaunchDescription([
-#         Node(
-#             package='four_now',
-#             executable='map_logger_node',
-#             name='map_logger_node',
-#             output='screen'
-#         )
-#     ])
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='four_now',
+            executable='map_logger_node',
+            name='map_logger_node',
+            output='screen'
+        )
+    ])
